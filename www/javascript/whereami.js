25f86cf7f3f13cdb11a6ec7e9de3078916fe1672
straup <straup@gmail.com>
2013-01-29 14:25:59 +0100

    start working on the round-trip...

---
+++ www/javascript/whereami.js
@@ -0,0 +1,32 @@
+function whereami_locate(){
+
+	var set_status = function(msg){
+		var status = $("#status");
+		status.html("<li>" + msg + "</li>" + status.html());
+	};
+
+	var _onsuccess = function(rsp){
+
+		var coords = rsp['coords'];
+
+		// trim me...
+		var lat = coords['latitude'];
+		var lon = coords['longitude'];
+
+		set_status("The sensors have placed you at or around " + lat + "," + lon + ".");
+		set_status("Now to figure out where that is.");
+
+		setTimeout(function(){
+			location.href = location.href + "?lat=" + lat + "&" + "lon=" + lon;
+		}, 2500);
+	};
+
+	var _onerror = function(rsp){
+		console.log("error");
+		console.log(rsp);
+	};
+
+	navigator.geolocation.getCurrentPosition(_onsuccess, _onerror);
+
+	set_status("Asking the sky where you are.");
+}
--- www/whereami.php
+++ www/whereami.php
@@ -1,16 +1,46 @@
 <?php
 
 	include("include/init.php");
-
 	loadlib("reverse_geocode");
+
+	# TO DO: ensure logged in
 
 	$lat = request_float("lat");
 	$lon = request_float("lon");
 
 	if (($lat) && ($lon)){
+
+		# TO DO: validate lat,lon
+
+		$GLOBALS['smarty']->assign("latitude", $lat);
+		$GLOBALS['smarty']->assign("longitude", $lon);
+
+		# TO DO: choose endpoint by placetype
+
 		$rsp = reverse_geocode($lat, $lon);
-		$GLOBALS['smarty']->assign("step", "choose");
-		$GLOBALS['smarty']->assign_by_ref("rsp", $rsp);
+
+		if (post_isset("choose")){
+
+			$GLOBALS['smarty']->assign("step", "update");
+
+			# see also: artisanal integers
+			$choice = post_int64("whereami");
+			$ok = 0;
+
+			foreach ($rsp['data'] as $row){
+				if ($row['woe_id'] == $choice){
+					$ok = 1;
+					break;
+				}
+			}
+
+			# TO DO: finish error checking; update db...
+		}
+
+		else {
+			$GLOBALS['smarty']->assign("step", "choose");
+			$GLOBALS['smarty']->assign_by_ref("rsp", $rsp);
+		}
 	}
 
 	$GLOBALS['smarty']->display("page_whereami.txt");
