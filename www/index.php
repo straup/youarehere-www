7f23115e405591f3427e80cad7baeb11b0b38bbc
straup <straup@gmail.com>
2013-02-02 06:17:05 +0100

    move stuff on to  the front page

--- www/index.php
+++ www/index.php
@@ -1,32 +1,80 @@
 <?
-	#
-	# $Id$
-	#
 
 	include('include/init.php');
 
+	if (! $GLOBALS['cfg']['user']){
 
-	#
-	# this is so we can test the logging output
-	#
-
-	if ($_GET['log_test']){
-		log_error("This is an error!");
-		log_fatal("Fatal error!");
+		$GLOBALS['smarty']->display("page_index_logged_out.txt");
+		exit();
 	}
 
+	loadlib("reverse_geocode");
+	loadlib("corrections");
 
-	#
-	# this is so we can test the HTTP library
-	#
+	$lat = request_float("lat");
+	$lon = request_float("lon");
+	$filter = $GLOBALS['cfg']['reverse_geocode_default_filter'];
 
-	if ($_GET['http_test']){
-		$ret = http_get("http://google.com");
+	if ($f = request_str("filter")){
+		$filter = $f;
 	}
 
+	if (($lat) && ($lon)){
 
-	#
-	# output
-	#
+		# TO DO: validate lat,lon
 
-	$smarty->display('page_index.txt');
+		$GLOBALS['smarty']->assign("latitude", $lat);
+		$GLOBALS['smarty']->assign("longitude", $lon);
+
+		# TO DO: choose endpoint by placetype
+
+		$rsp = reverse_geocode($lat, $lon, $filter);
+
+		if (post_isset("choose")){
+
+			$GLOBALS['smarty']->assign("step", "update");
+
+			# TO DO: something with $choice is -1
+
+			# see also: artisanal integers
+			$choice = post_int64("whereami");
+			$ok = 0;
+
+			foreach ($rsp['data'] as $row){
+				if ($row['woe_id'] == $choice){
+					$ok = 1;
+					break;
+				}
+			}
+
+			if ($ok){
+
+				$correction = array(
+					'latitude' => $lat,
+					'longitude' => $lon,
+					'woe_id' => $choice,
+				);
+
+				$perspective = post_int32("perspective");
+
+				if (($perspective) && (corrections_is_valid_perspective($perspective))){
+					$correction['perspective'] = $perspective;
+				}
+
+				$rsp = corrections_add_correction($correction);
+			}
+
+			# TO DO: notifications (pubsub or ... ?)
+		}
+
+		else {
+			$GLOBALS['smarty']->assign("step", "choose");
+			$GLOBALS['smarty']->assign_by_ref("rsp", $rsp);
+		}
+	}
+
+	$map = corrections_perspective_map();
+	$GLOBALS['smarty']->assign_by_ref("perspective_map", $map);
+
+	$GLOBALS['smarty']->display("page_index.txt");
+	exit();
